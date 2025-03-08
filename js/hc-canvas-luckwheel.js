b7aa77822b239f557fb150cbc74390976ddb6c75
Triethuynh2202 <85531802+Triethuynh2202@users.noreply.github.com>
2025-03-08 14:12:13 +0700

    Update hc-canvas-luckwheel.js

--- js/hc-canvas-luckwheel.js
+++ js/hc-canvas-luckwheel.js
@@ -1,28 +1,67 @@
 (function() {
-  var $, ele, container, canvas, num, prizes, btn, deg = 0, fnGetPrize, fnGotBack, optsPrize;
+  var $,
+    ele,
+    container,
+    canvas,
+    num,
+    prizes,
+    btn,
+    deg = 0,
+    fnGetPrize,
+    fnGotBack,
+    optsPrize;
 
-  var cssPrefix, eventPrefix, vendors = { "": "", Webkit: "webkit", Moz: "", O: "o", ms: "ms" },
-      testEle = document.createElement("p"), cssSupport = {};
+  var cssPrefix,
+    eventPrefix,
+    vendors = {
+      "": "",
+      Webkit: "webkit",
+      Moz: "",
+      O: "o",
+      ms: "ms"
+    },
+    testEle = document.createElement("p"),
+    cssSupport = {};
 
   Object.keys(vendors).some(function(vendor) {
-    if (testEle.style[vendor + (vendor ? "T" : "t") + "ransitionProperty"] !== undefined) {
+    if (
+      testEle.style[vendor + (vendor ? "T" : "t") + "ransitionProperty"] !==
+      undefined
+    ) {
       cssPrefix = vendor ? "-" + vendor.toLowerCase() + "-" : "";
       eventPrefix = vendors[vendor];
       return true;
     }
   });
 
+  /**
+   * @param  {[type]} name [description]
+   * @return {[type]}      [description]
+   */
   function normalizeEvent(name) {
     return eventPrefix ? eventPrefix + name : name.toLowerCase();
   }
 
+  /**
+   * @param  {[type]} name [description]
+   * @return {[type]}      [description]
+   */
   function normalizeCss(name) {
-    return cssPrefix ? cssPrefix + name.toLowerCase() : name;
+    name = name.toLowerCase();
+    return cssPrefix ? cssPrefix + name : name;
   }
 
-  cssSupport = { cssPrefix: cssPrefix, transform: normalizeCss("Transform"), transitionEnd: normalizeEvent("TransitionEnd") };
+  cssSupport = {
+    cssPrefix: cssPrefix,
+    transform: normalizeCss("Transform"),
+    transitionEnd: normalizeEvent("TransitionEnd")
+  };
+
   var transform = cssSupport.transform;
   var transitionEnd = cssSupport.transitionEnd;
+
+  // alert(transform);
+  // alert(transitionEnd);
 
   function init(opts) {
     fnGetPrize = opts.getPrize;
@@ -35,53 +74,109 @@
     events();
   }
 
-  $ = function(id) { return document.getElementById(id); };
+  /**
+   * @param  {String} id
+   * @return {Object} HTML element
+   */
+  $ = function(id) {
+    return document.getElementById(id);
+  };
 
   function draw(opts) {
+    opts = opts || {};
     if (!opts.id || num >>> 0 === 0) return;
 
-    var id = opts.id, rotateDeg = 360 / num / 2 + 90, ctx, prizeItems = document.createElement("ul"), turnNum = 1 / num, html = [];
+    var id = opts.id,
+      rotateDeg = 360 / num / 2 + 90,
+      ctx,
+      prizeItems = document.createElement("ul"),
+      turnNum = 1 / num,
+      html = [];
+
     ele = $(id);
     canvas = ele.querySelector(".hc-luckywheel-canvas");
     container = ele.querySelector(".hc-luckywheel-container");
     btn = ele.querySelector(".hc-luckywheel-btn");
 
-    if (!canvas.getContext) { alert("Browser is not supported"); return; }
+    if (!canvas.getContext) {
+      showMsg("Browser is not support");
+      return;
+    }
+
     ctx = canvas.getContext("2d");
 
     for (var i = 0; i < num; i++) {
       ctx.save();
       ctx.beginPath();
-      ctx.translate(250, 250);
+      ctx.translate(250, 250); // Center Point
       ctx.moveTo(0, 0);
       ctx.rotate((((360 / num) * i - rotateDeg) * Math.PI) / 180);
-      ctx.arc(0, 0, 250, 0, (2 * Math.PI) / num, false);
-      ctx.fillStyle = i % 2 == 0 ? "#0000FF" : "#FFFFFF";
+      ctx.arc(0, 0, 250, 0, (2 * Math.PI) / num, false); // Radius
+      if (i % 2 == 0) {
+          ctx.fillStyle = "#0000FF"; // Màu xanh
+      } else {
+          ctx.fillStyle = "#FFFFFF"; // Màu trắng
+      }
       ctx.fill();
       ctx.lineWidth = 1;
       ctx.strokeStyle = "#e4370e";
       ctx.stroke();
       ctx.restore();
-
       var prizeList = opts.prizes;
-      let textColor = i % 2 === 0 ? "#FFFF00" : "#0000FF";
-      html.push('<li class="hc-luckywheel-item"><span style="' + transform + ': rotate(' + i * turnNum + 'turn)">');
-      html.push('<p id="curve" style="color: ' + textColor + ';">' + prizeList[i].text + '</p>');
-      if (prizeList[i].img) html.push('<img src="' + prizeList[i].img + '" />');
-      html.push('</span></li>');
+      html.push('<li class="hc-luckywheel-item"> <span style="');
+      html.push(transform + ": rotate(" + i * turnNum + 'turn)">');
+      if (opts.mode == "both") {
+          html.push("<p id='curve'>" + prizeList[i].text + "</p>");
+          html.push('<img src="' + prizeList[i].img + '" />');
+      } else if (prizeList[i].img) {
+          html.push('<img src="' + prizeList[i].img + '" />');
+      } else {
+          html.push('<p id="curve">' + prizeList[i].text + "</p>");
+      }
+      html.push("</span> </li>");
+      if (i + 1 === num) {
+        prizeItems.className = "hc-luckywheel-list";
+        container.appendChild(prizeItems);
+        prizeItems.innerHTML = html.join("");
+      }
     }
-    prizeItems.className = "hc-luckywheel-list";
-    container.appendChild(prizeItems);
-    prizeItems.innerHTML = html.join("");
   }
 
-  function runRotate(deg) { container.style[transform] = "rotate(" + deg + "deg)"; }
+  /**
+   * @param  {String} msg [description]
+   */
+  function showMsg(msg) {
+    alert(msg);
+  }
 
+  /**
+   * @param  {[type]} deg [description]
+   * @return {[type]}     [description]
+   */
+  function runRotate(deg) {
+    // runInit();
+    // setTimeout(function() {
+    container.style[transform] = "rotate(" + deg + "deg)";
+    // }, 10);
+  }
+
+  /**
+   * @return {[type]} [description]
+   */
   function events() {
     bind(btn, "click", function() {
+
       addClass(btn, "disabled");
+
       fnGetPrize(function(data) {
-        optsPrize = { prizeId: data[0], chances: data[1] };
+        if (data[0] == null && !data[1] == null) {
+          return;
+        }
+        optsPrize = {
+          prizeId: data[0],
+          chances: data[1]
+        };
+        deg = deg || 0;
         deg = deg + (360 - (deg % 360)) + (360 * 10 - data[0] * (360 / num));
         runRotate(deg);
       });
@@ -90,20 +185,78 @@
   }
 
   function eGot() {
-    removeClass(btn, "disabled");
-    return fnGotBack(prizes[optsPrize.prizeId].text);
+    if (optsPrize.chances == null) {
+      return fnGotBack(null);
+    } else {
+      removeClass(btn, "disabled");
+      return fnGotBack(prizes[optsPrize.prizeId].text);
+    }
   }
 
+  /**
+   * Bind events to elements
+   * @param {Object}    ele    HTML Object
+   * @param {Event}     event  Event to detach
+   * @param {Function}  fn     Callback function
+   */
   function bind(ele, event, fn) {
-    if (typeof addEventListener === "function") ele.addEventListener(event, fn, false);
-    else if (ele.attachEvent) ele.attachEvent("on" + event, fn);
+    if (typeof addEventListener === "function") {
+      ele.addEventListener(event, fn, false);
+    } else if (ele.attachEvent) {
+      ele.attachEvent("on" + event, fn);
+    }
   }
 
-  function hasClass(ele, cls) { return ele.classList ? ele.classList.contains(cls) : ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)")); }
-  function addClass(ele, cls) { if (!hasClass(ele, cls)) ele.className += " " + cls; }
-  function removeClass(ele, cls) { ele.className = ele.className.replace(new RegExp("(^|\\b)" + cls + "(\\b|$)", "gi"), " "); }
+  /**
+   * hasClass
+   * @param {Object} ele   HTML Object
+   * @param {String} cls   className
+   * @return {Boolean}
+   */
+  function hasClass(ele, cls) {
+    if (!ele || !cls) return false;
+    if (ele.classList) {
+      return ele.classList.contains(cls);
+    } else {
+      return ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
+    }
+  }
 
-  var hcLuckywheel = { init: function(opts) { return init(opts); } };
+  // addClass
+  function addClass(ele, cls) {
+    if (ele.classList) {
+      ele.classList.add(cls);
+    } else {
+      if (!hasClass(ele, cls)) ele.className += "" + cls;
+    }
+  }
+
+  // removeClass
+  function removeClass(ele, cls) {
+    if (ele.classList) {
+      ele.classList.remove(cls);
+    } else {
+      ele.className = ele.className.replace(
+        new RegExp(
+          "(^|\\b)" + className.split(" ").join("|") + "(\\b|$)",
+          "gi"
+        ),
+        " "
+      );
+    }
+  }
+
+  var hcLuckywheel = {
+    init: function(opts) {
+      return init(opts);
+    }
+  };
+
   window.hcLuckywheel === undefined && (window.hcLuckywheel = hcLuckywheel);
-  if (typeof define == "function" && define.amd) define("HellCat-Luckywheel", [], function() { return hcLuckywheel; });
+
+  if (typeof define == "function" && define.amd) {
+    define("HellCat-Luckywheel", [], function() {
+      return hcLuckywheel;
+    });
+  }
 })();
