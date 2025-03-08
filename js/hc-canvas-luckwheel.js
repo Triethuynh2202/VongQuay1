c3b33eb3d009ff762e7ed3d7810b342658f4f7d9
Triethuynh2202 <85531802+Triethuynh2202@users.noreply.github.com>
2025-03-08 15:12:05 +0700

    Update hc-canvas-luckwheel.js

--- js/hc-canvas-luckwheel.js
+++ js/hc-canvas-luckwheel.js
@@ -49,7 +49,7 @@
     ctx = canvas.getContext("2d");
     
     for (var i = 0; i < num; i++) {
-      ctx.save(); ctx.beginPath(); ctx.translate(300, 300); ctx.moveTo(0, 0);
+      ctx.save(); ctx.beginPath(); ctx.translate(320, 300); ctx.moveTo(0, 0);
       ctx.rotate((((360 / num) * i - rotateDeg) * Math.PI) / 180);
       ctx.arc(0, 0, 250, 0, (2 * Math.PI) / num, false);
       ctx.fillStyle = i % 2 == 0 ? "#0000FF" : "#FFFFFF";
